2cc30c4c2fa1657a4249b7547a624ecc57266a81
Dylan DesRosier <dylan@pooltogether.com>
2021-09-07 15:14:17 -0400

    conflict

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+import schema from './v4-pool-data.schema.json'
+
+export * from './types'
+
+export { schema }
