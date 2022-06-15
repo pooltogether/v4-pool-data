e03425c6a3878398d699a9118a500d1400fdb269
Pierrick Turelier <pierrick@turelier.com>
2022-06-15 15:56:38 -0500

    chore(lint): setup prettier format

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import testnet from '@pooltogether/v4-testnet/contracts.json'
-import mainnet from '@pooltogether/v4-mainnet/contracts.json'
-export { testnet }
-export { mainnet }
+import testnet from '@pooltogether/v4-testnet/contracts.json';
+import mainnet from '@pooltogether/v4-mainnet/contracts.json';
+
+export { testnet };
+export { mainnet };
+
 export default {
   testnet,
   mainnet
-}
+};
