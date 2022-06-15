6622f5ca4047e6daebdbf44a8a7c87b0bfbfc225
Pierrick Turelier <pierrick@turelier.com>
2022-06-15 15:57:23 -0500

    fix(tests): fix mainnet tests

--- test/validate.test.ts
+++ test/validate.test.ts
@@ -1,23 +1,20 @@
-import testData from '../src/test.json'
-import testSchema from '../src/test.schema.json'
-import { schema } from '@pooltogether/contract-list-schema'
-import testnetData from '../src/testnet.json'
-import AJV from 'ajv'
+import AJV from 'ajv';
+import { schema } from '@pooltogether/contract-list-schema';
 
-describe('validate test data against schema', () => {
-  it('data is valid', () => {
-    const ajv = new AJV()
-    expect(ajv.validate(testSchema, testData)).toBeTruthy()
-  })
-})
+import { mainnet, testnet } from '../src/index';
 
-describe('validate testnet against schema', () => {
-  it('data is valid', () => {
-    const ajv = new AJV()
-    const isValid = ajv.validate(schema, testnetData)
-    if (ajv.errors) {
-      console.warn(ajv.errors)
-    }
-    expect(isValid).toBeTruthy()
-  })
-})
+describe('validate data against schema', () => {
+  let ajv: AJV;
+
+  beforeEach(() => {
+    ajv = new AJV();
+  });
+
+  it('validate testnet data', () => {
+    expect(ajv.validate(schema, testnet)).toBeTruthy();
+  });
+
+  it('validate mainnet data', () => {
+    expect(ajv.validate(schema, mainnet)).toBeTruthy();
+  });
+});
