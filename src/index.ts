a1e2e7379ca6549e8dae25e636b2c6b67ba85bf9
Pierrick Turelier <pierrick@turelier.com>
2022-06-16 10:43:39 -0500

    feat(data): generate prize pool network datas

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
-import testnet from '@pooltogether/v4-testnet/contracts.json';
 import mainnet from '@pooltogether/v4-mainnet/contracts.json';
+import testnet from '@pooltogether/v4-testnet/contracts.json';
+import tokenomicsTesnet from '@pooltogether/tokenomics-testnet/contracts.json';
 
-export { testnet };
+import generatePrizePoolNetwork from './utils/generatePrizePoolNetwork';
+
+const prizePoolNetworkTestnet = generatePrizePoolNetwork(testnet, tokenomicsTesnet);
+
 export { mainnet };
+export { testnet };
+export { tokenomicsTesnet };
+export { prizePoolNetworkTestnet };
 
 export default {
+  mainnet,
   testnet,
-  mainnet
+  tokenomicsTesnet,
+  prizePoolNetworkTestnet
 };
