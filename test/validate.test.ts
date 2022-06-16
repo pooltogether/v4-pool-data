6250a7cc120562b0a69cd56a23bc9fabf933cf9a
Pierrick Turelier <pierrick@turelier.com>
2022-06-16 10:43:39 -0500

    feat(data): generate prize pool network datas

--- test/validate.test.ts
+++ test/validate.test.ts
@@ -1,7 +1,7 @@
 import AJV from 'ajv';
 import { schema } from '@pooltogether/contract-list-schema';
 
-import { mainnet, testnet } from '../src/index';
+import { mainnet, testnet, tokenomicsTesnet, prizePoolNetworkTestnet } from '../src/index';
 
 describe('validate data against schema', () => {
   let ajv: AJV;
@@ -12,6 +12,14 @@ describe('validate data against schema', () => {
 
   it('validate testnet data', () => {
     expect(ajv.validate(schema, testnet)).toBeTruthy();
+  });
+
+  it('validate tokenomicsTesnet data', () => {
+    expect(ajv.validate(schema, tokenomicsTesnet)).toBeTruthy();
+  });
+
+  it('validate prizePoolNetworkTestnet data', () => {
+    expect(ajv.validate(schema, prizePoolNetworkTestnet)).toBeTruthy();
   });
 
   it('validate mainnet data', () => {
